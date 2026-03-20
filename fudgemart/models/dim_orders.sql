4d5a0bfc5d9782f8eec8165b02bd959fca903fbc
Jelena Mlikota <jmlikota@g.syr.edu>
2026-03-20 18:46:04 +0000

    added .yml files for dim_date and dim_orders and deleted schema.yml

--- fudgemart/models/dim_orders.sql
+++ fudgemart/models/dim_orders.sql
@@ -1,11 +1,10 @@
 with
--- Stage Fudgemart orders
 stg_fm_orders as (
     select
         order_id,
-        {{ dbt_utils.generate_surrogate_key(['order_id']) }} as orderkey,
-        customer_id as customerkey,
         'Fudgemart' as division,
+        {{ dbt_utils.generate_surrogate_key(['order_id', 'division']) }} as orderkey,
+        customer_id as customerkey,
         replace(to_date(order_date)::varchar, '-', '')::int as orderdatekey,
         replace(to_date(shipped_date)::varchar, '-', '')::int as shippeddatekey,
         null as returneddatekey,
@@ -13,13 +12,12 @@ stg_fm_orders as (
     from {{ source('fudgemart_v3','fm_orders') }}
 ),
 
--- Stage Fudgeflix orders
 stg_ff_orders as (
     select
-        at_id as order_id,
-        {{ dbt_utils.generate_surrogate_key(['at_id']) }} as orderkey,
-        at_account_id as customerkey,
+        at_id AS order_id,
         'Fudgeflix' as division,
+        {{ dbt_utils.generate_surrogate_key(['at_id', 'division']) }} as orderkey,
+        at_account_id as customerkey,
         replace(to_date(at_queue_date)::varchar, '-', '')::int as orderdatekey,
         replace(to_date(at_shipped_date)::varchar, '-', '')::int as shippeddatekey,
         replace(to_date(at_returned_date)::varchar, '-', '')::int as returneddatekey,
@@ -27,7 +25,6 @@ stg_ff_orders as (
     from {{ source('fudgeflix_v3','ff_account_titles') }}
 ),
 
--- Combine both sources
 all_orders as (
     select * from stg_fm_orders
     union all
